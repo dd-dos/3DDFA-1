3dab6121cbde8295797acc3f609aa14877e4b3dd
dd-dos <dungphamdang99@gmail.com>
2021-08-26 04:44:46 +0000

    Move train script

---
+++ scripts/train.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+LOG_ALIAS=$1
+LOG_DIR="logs"
+mkdir -p ${LOG_DIR}
+
+LOG_FILE="${LOG_DIR}/${LOG_ALIAS}_`date +'%Y-%m-%d_%H:%M.%S'`.log"
+#echo $LOG_FILE
+
+python3 train.py --arch="mobilenet_1" \
+    --loss=wpdc \
+    --snapshot="snapshot/phase1_wpdc" \
+    --opt-style=resample \
+    --resample-num=132 \
+    --train-batch-size=128 \
+    --val-batch-size=128 \
+    --base-lr=1e-4 \
+    --epochs=9999 \
+    --devices-id=0 \
+    --workers=12 \
+    --log-file="${LOG_FILE}" \
+    --train-path 'data/300VW-3D_cropped_closed_eyes_3ddfa' \
+                 'data/300VW-3D_cropped_closed_eyes_GAN_3ddfa' \
+                 'data/300VW-3D_cropped_opened_eyes_3ddfa' \
+                 'data/300WLP_3ddfa' \
+    --val-path 'data/AFLW2000_3ddfa' \
+    --use-amp \
+    --beta=0.8
