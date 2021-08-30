2eb67e2bcff3f21839c32008ffca09965c48af0a
pdd-99 <gran@techainer.com>
2021-08-30 22:39:22 +0700

    Update training pipeline

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,18 +12,20 @@ python3 train.py --arch="mobilenet_1" \
     --snapshot="snapshot/phase1_wpdc" \
     --opt-style=resample \
     --resample-num=132 \
-    --train-batch-size=64 \
+    --train-batch-size=128 \
     --val-batch-size=128 \
     --base-lr=1e-4 \
     --epochs=9999 \
     --devices-id=0 \
-    --workers=4 \
+    --workers=8 \
     --log-file="${LOG_FILE}" \
-    --train-path 'data/300VW-3D_cropped_closed_eyes_3ddfa' \
-                 'data/300VW-3D_cropped_closed_eyes_GAN_3ddfa' \
-                 'data/300VW-3D_cropped_opened_eyes_3ddfa' \
+    --train-path 'data/300VW-3D_closed_eyes_3ddfa' \
+                 'data/300VW-3D_opened_eyes_3ddfa' \
                  'data/300WLP_3ddfa' \
     --val-path 'data/AFLW2000_3ddfa' \
+    --resume 'snapshot/2021-08-30/wpdc_best.pth.tar' \
     --use-amp \
-    --resume 'snapshot/wpdc_last.pth.tar' \
-    --beta=0.8
+    --optimizer 'sgd' \
+    --use-scheduler \
+    --beta=0.8 \
+    --num-log-samples 16
