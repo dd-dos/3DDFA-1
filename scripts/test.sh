ac4fe55efb64a39b88073605a72824b35214488a
pdd <gran@techainer.com>
2021-09-03 16:13:28 +0700

    Infer

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,5 @@ python3 inference.py --mode='img' \
                  --img-path='samples/0386.jpg' \
                  --input-size=128 \
                  --num-classes=101 \
-                 --img-path='samples/bed2.jpg' \
+                 --img-path='samples/sleep1.jpg' \
                  --video-path='samples/e7fcc117-8877-4902-815c-cb079cd62b88__MmoYJ.mov' \
-                 --save-path='inference_results/e7fcc117-8877-4902-815c-cb079cd62b88__MmoYJ.mp4'
