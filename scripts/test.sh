a813de2805a8eff8a54fbdbade9ce93e9ff171d6
pdd <gran@techainer.com>
2021-09-13 08:32:56 +0700

    Minor fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 python3 inference.py --mode='video' \
                  --model-path='snapshot/2021-07-22/last.pth.tar' \
-                 --video-path='samples/607a06fc-7e8f-4113-81f8-956c1b922dc1_5.mov'
+                 --save-path='foo.mp4'
